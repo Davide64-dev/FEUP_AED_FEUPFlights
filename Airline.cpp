67532cba610f815ee5dbc9d5d7e4d5a46f1ac89a
Davide Teixeira <davidpcu@hotmail.com>
2023-01-05 13:28:22 +0000

    Documentei a classe Airline

--- Airline.cpp
+++ Airline.cpp
@@ -4,21 +4,44 @@
 
 #include "Airline.h"
 
+/**
+ * Construtor da classe Airline
+ * @param cod Código ICAO
+ * @param name Nome "oficial"
+ * @param callSign "Alcunha"
+ * @param country País de origem
+ */
 Airline::Airline(string cod, string name, string callSign, string country):
     cod(cod), name(name), callSign(callSign), country(country){};
 
+/**
+ * Complexidade: O(1)
+ * @return Retorna o código ICAO da companhia aérea
+ */
 string Airline::getCod() const {
     return cod;
 }
 
+/**
+ * Complexidade: O(1)
+ * @return Retorna o nome da companhia aérea
+ */
 string Airline::getName() const {
     return name;
 }
 
+/**
+ * Complexiade: O(1)
+ * @return Retorna a "alcunha" da companhia aérea
+ */
 string Airline::getCallSign() const {
     return callSign;
 }
 
+/**
+ * Complexidade: O(1)
+ * @return Retorna o país de origem da companhia aérea
+ */
 string Airline::getCountry() const {
     return country;
 }
--- Airline.h
+++ Airline.h
@@ -9,11 +9,28 @@
 
 using namespace std;
 
+/**
+ * Esta classe pretende representar uma companhia aérea
+ */
 class Airline {
-
+    /**
+     * Código ICAO da companhia aérea
+     */
     string cod;
+
+    /**
+     * Nome da companhia aérea
+     */
     string name;
+
+    /**
+     * "Alcunha" da companhia aérea
+     */
     string callSign;
+
+    /**
+     * País de origem
+     */
     string country;
 
 public:
@@ -24,9 +41,18 @@ public:
     string getCountry() const;
 };
 
+/**
+ * Struct auxiliar utilizada no unordered_set "airlines" na classe "FeupFlights"
+ */
 struct HashAirline
 {
-    // TODO
+    /**
+     * Hash Function a ser utilizada no unordered_set\n
+     * A Hash function recebe o código da companhia aérea (3 letras) e gera um número inteiro que consiste
+     * na concatenação dos códigos ASCII de cada uma das companhias, gerando um número inteiro único para cada código
+     * @param b Companhia aérea sobre a qual irá ser aplicada a função hash
+     * @return Valor inteiro proveniente da concatenação dos códigos ASCII de cada um dos caracteres
+     */
     int operator() (const Airline& b) const {
         string temp = "";
         for (unsigned i = 0; i < 3;i++){
@@ -36,7 +62,14 @@ struct HashAirline
         return stoi(temp);
     }
 
-    // TODO
+    /**
+     * Equality Function a ser utilizada no unordered_set\n
+     * A Equality Function recebe duas companhias aéreas e retorna a igualdade como verdadeira se e somente se
+     *  os seus códiso ICAO sejam iguais
+     * @param b1 Primeira Companhia Aérea
+     * @param b2 Segunda Companhia Aérea
+     * @return Valor boolean sobre a igualdade, ou diferença, das companhias aéreas
+     */
     bool operator() (const Airline& b1, const Airline& b2) const {
         return b1.getCod() == b2.getCod();
     }
